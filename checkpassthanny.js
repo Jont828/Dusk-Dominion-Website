3a3a744f525e740969c0b1bbfd687a0eef748029
Jont828 <Jxtdragon@gmail.com>
2016-04-25 17:14:36 -0400

    Initial commit

---
+++ checkpassthanny.js
@@ -0,0 +1,7 @@
+function validatePass(){
+    if(document.getElementById('password').value == 'tomcruise'){
+        window.open('https://docs.google.com/spreadsheets/d/1uuc0Xu7IxYLksbEbd0QxzPQWx2uHbYZnihh3T6q8ZXs/edit?disco=AAAAAjFVv28');
+    } else {
+        alert('Wrong password!!');
+    }
+}
